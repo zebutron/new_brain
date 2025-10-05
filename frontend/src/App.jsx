11957fa841990416b4600fd8db7b5801a04a582e
Zebulon Reynolds <zebulonreynolds@gmail.com>
2025-10-05 03:05:49 -0600

    Explicit 4-bar A/B with angle brackets
    Using <pattern1 pattern2> structure:
    - <[a1...]*4 [e1...]*4> - A bass 4x, then E bass 4x
    - <...!4 ...!4> - parameters held for 4 bars each
    - <<melodyA> <melodyB>> - melody switches properly
    
    Each element explicitly defined for 4-bar sections
    SHOULD WORK NOW

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,65 +5,36 @@ import { useStrudelDirect } from './hooks/useStrudelDirect'
 import './App.css'
 
 function App() {
-  const [code, setCode] = useState(`slowcat(
-  stack(
-    note("a1 bb1 a1 ab1")
-      .s("sawtooth")
-      .lpf(800)
-      .resonance(15)
-      .distort(0.8)
-      .room(0.7)
-      .decay(0.8)
-      .gain(1.3),
-    note("a0 bb0 a0 ab0")
-      .s("sine")
-      .lpf(200)
-      .distort(0.3)
-      .gain(1.0),
-    s("bd ~ ~ bd, ~ ~ sd ~")
-      .gain(1.4)
-      .room(0.8),
-    s("~ ~ ~ ~, [~ ~ hh ~]*2")
-      .gain(0.6),
-    note("[~ ~ a4 ~] [~ c5 ~ ~] [~ ~ eb5 ~] [d5 ~ ~ ~]")
-      .s("sawtooth")
-      .lpf(sine.range(800, 2000).slow(4))
-      .distort(sine.range(0.3, 0.7).slow(3))
-      .gain(sine.range(0.6, 0.9).slow(2.5))
-      .decay(0.6)
-      .room(0.8)
-  ).segment(4),
-  stack(
-    note("e1 f1 e1 eb1")
-      .s("sawtooth")
-      .lpf(600)
-      .resonance(18)
-      .distort(0.9)
-      .room(0.8)
-      .decay(0.9)
-      .gain(1.5),
-    note("e0 f0 e0 eb0")
-      .s("sine")
-      .lpf(150)
-      .distort(0.4)
-      .gain(1.2),
-    note("e-1 f-1 e-1 eb-1")
-      .s("sine")
-      .lpf(80)
-      .gain(0.8),
-    s("bd ~ ~ bd, ~ ~ sd ~")
-      .gain(1.5)
-      .room(0.9),
-    s("~ ~ ~ ~, [~ ~ hh ~]*2")
-      .gain(0.5),
-    note("[e4 f4 ~ gb4] [~ ab4 f4 ~] [e4 ~ eb4 db4] [c4 ~ db4 eb4]")
-      .s("sawtooth")
-      .lpf(sine.range(600, 1800).slow(4))
-      .distort(sine.range(0.5, 0.9).slow(3))
-      .gain(sine.range(0.7, 1.0).slow(2))
-      .decay(0.4)
-      .room(0.9)
-  ).segment(4)
+  const [code, setCode] = useState(`stack(
+  note("<[a1 bb1 a1 ab1]*4 [e1 f1 e1 eb1]*4>")
+    .s("sawtooth")
+    .lpf("<800!4 600!4>")
+    .resonance("<15!4 18!4>")
+    .distort("<0.8!4 0.9!4>")
+    .room("<0.7!4 0.8!4>")
+    .decay("<0.8!4 0.9!4>")
+    .gain("<1.3!4 1.5!4>"),
+  note("<[a0 bb0 a0 ab0]*4 [e0 f0 e0 eb0]*4>")
+    .s("sine")
+    .lpf("<200!4 150!4>")
+    .distort("<0.3!4 0.4!4>")
+    .gain("<1.0!4 1.2!4>"),
+  note("<~!4 [e-1 f-1 e-1 eb-1]*4>")
+    .s("sine")
+    .lpf(80)
+    .gain(0.8),
+  s("bd ~ ~ bd, ~ ~ sd ~")
+    .gain("<1.4!4 1.5!4>")
+    .room("<0.8!4 0.9!4>"),
+  s("~ ~ ~ ~, [~ ~ hh ~]*2")
+    .gain("<0.6!4 0.5!4>"),
+  note("<<[~ ~ a4 ~] [~ c5 ~ ~] [~ ~ eb5 ~] [d5 ~ ~ ~]> <[e4 f4 ~ gb4] [~ ab4 f4 ~] [e4 ~ eb4 db4] [c4 ~ db4 eb4]>>")
+    .s("sawtooth")
+    .lpf(sine.range(800, 2000).slow(8))
+    .distort(sine.range(0.3, 0.7).slow(6))
+    .gain(sine.range(0.6, 0.9).slow(5))
+    .decay("<0.6!4 0.4!4>")
+    .room("<0.8!4 0.9!4>")
 )`)
   const { isPlaying, error, toggle } = useStrudelDirect()
 
