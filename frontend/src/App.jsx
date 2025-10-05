02cbb0133b44f56f9297458baf2d3c71b3063826
Zebulon Reynolds <zebulonreynolds@gmail.com>
2025-10-05 02:20:03 -0600

    Funky syncopated hats + memorable melody hook
    Hi-hats:
    - 4-bar phrase with 16th/8th syncopation
    - Sparse with funky accents
    - [hh hh] doubles for groove
    - Varied rhythm each bar
    
    Melody:
    - E-G-A-C-B-A-G-E phrase (memorable hook)
    - Sustained notes with space
    - Builds to C6, resolves down
    - Clean, catchy, repeatable
    - No random chaos
    
    ALL DOCS UPDATED FOR FUTURE AI AGENTS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,17 +14,17 @@ function App() {
     .sometimes(x => x.add(note(12))),
   s("bd*4, ~ sd ~ <sd sd sd [sd ~ ~ sd]>")
     .gain(1.2),
-  s("<[hh ~ hh [hh hh]*2] [~ hh ~ [hh hh hh ~]] [[hh ~]*2 hh ~] [~ [hh hh] ~ hh]>")
-    .gain(0.75),
+  s("<[hh ~ hh ~] [~ [hh hh] ~ hh] [hh ~ ~ [hh ~ hh ~]] [~ hh [hh ~ hh] ~]>")
+    .gain(0.7),
   s("~ ~ ~ cp")
     .gain(0.9)
     .delay(0.4)
     .delayfeedback(0.3),
-  note("<[~ e5 ~ [g5 a5]*2] [c6 ~ b5 ~] [~ ~ a5 g5] [[e5 g5] ~ c6 ~]>")
+  note("<[e5 ~ ~ e5] [g5 ~ a5 ~] [~ c6 ~ b5] [a5 ~ g5 e5]>")
     .s("square")
     .lpf(5000)
-    .decay(0.12)
-    .gain(0.65)
+    .decay(0.15)
+    .gain(0.7)
     .delay(0.4)
     .delaytime(0.0833)
     .delayfeedback(0.25)
